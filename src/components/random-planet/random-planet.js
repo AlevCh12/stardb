086adf0a5a5d6df896b42eebd51c1e2d3420b4e2
AlevCh12 <amirgaliev.2016@mail.ru>
2025-02-26 14:24:26 +0600

    step 5 added person details

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -15,10 +15,17 @@ export default class RandomPlanet extends Component {
         error: false
     };
 
-    constructor(props) {
-        super(props)
+
+        componentDidMount() {
         this.updatePlanet()
+            this.interval = setInterval(this.updatePlanet, 3000)
+        }
+
+        componentWillUnmount() {
+            clearInterval(this.interval)
+
     }
+
 
     onPlanetLoaded = (planet) => {
         this.setState({
@@ -34,7 +41,10 @@ export default class RandomPlanet extends Component {
         });
     }
     updatePlanet = () => {
-        const id = Math.floor(Math.random()*15) + 2;
+        let id = 20;
+        while (id === 20 ) {
+            id = Math.floor(Math.random()*19) + 3
+        }
         this.swapiService
             .getPlanet(id)
             .then(this.onPlanetLoaded)
