28d7050bf7bafb6dfe80bbc820645cd784fe7c66
AlevCh12 <amirgaliev.2016@mail.ru>
2025-03-26 12:21:10 +0600

    Step 11 upd

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,8 +3,6 @@ import React, {Component} from 'react'
 
 import Header from '../header'
 import RandomPlanet from '../random-planet'
-import ItemList from '../item-list'
-import PersonDetails from '../person-details'
 import ErrorIndicator from "../error-indicator";
 import PeoplePage from "../people-page";
 import SwapiService from "../../services/swapi-service";
@@ -32,7 +30,7 @@ export default class App extends Component{
         return (
             <div>
                 <Header/>
-                <RandomPlanet/>
+                <RandomPlanet />
                 <PeoplePage/>
             </div>
         )
