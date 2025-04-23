8d2e300e7699cbb2467c1a95d84d11b986a21bf9
AlevCh12 <amirgaliev.2016@mail.ru>
2025-04-23 10:10:10 +0600

    Step 8 small changes

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,14 +4,19 @@ import Header from '../header'
 import RandomPlanet from '../random-planet'
 import ItemList from '../item-list'
 import PersonDetails from '../person-details'
+import SwapiService from "../../services/swapi-service";
 
 import './app.css'
 import ErrorIndicator from "../error-indicator";
 import PeoplePage from "../people-page";
 
 export default class App extends Component{
+
+    swapiService = new SwapiService()
+
     state = {
-        selectedPerson: null
+        selectedPerson: null,
+        hasError: false
     }
     componentDidCatch(error, errorInfo) {
         this.setState({ hasError: true })
@@ -25,7 +30,29 @@ export default class App extends Component{
             <div>
                 <Header/>
                 <RandomPlanet/>
-                <PeoplePage />
+                <PeoplePage/>
+
+                <div className="row mb2">
+                    <div className="col-md-6">
+                        <ItemList
+                            onItemSelected={this.onPersonSelected}
+                            getData={this.swapiService.getAllPlanets}/>
+                    </div>
+                    <div className="col-md-6">
+                        <PersonDetails personId={this.state.selectedPerson}/>
+                    </div>
+                </div>
+
+                <div className="row mb2">
+                    <div className="col-md-6">
+                        <ItemList
+                            onItemSelected={this.onPersonSelected}
+                            getData={this.swapiService.getAllStarships}/>
+                    </div>
+                    <div className="col-md-6">
+                        <PersonDetails personId={this.state.selectedPerson}/>
+                    </div>
+                </div>
             </div>
         )
     }
